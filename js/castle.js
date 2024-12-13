0a039c5796527dd42bdc14dac25249eecc466847
Ma1n <kskiller2020@gmail.com>
2024-12-13 21:52:43 +0200

    add buff logic

--- js/castle.js
+++ js/castle.js
@@ -3,6 +3,7 @@ class Character {
     constructor(name, health = 100) {
         this.healt = health
         this.name = name;
+        this.isAlive = true;
     }
 }
 class Glory extends Character {
@@ -13,13 +14,10 @@ class Glory extends Character {
     damage() {
         if(this.healt - 15 < 0){
             this.healt = 0
+            this.isAlive = false
             return
         }
-        if (!imgSword.classList.contains("attack")) {
-            this.healt -= 15
-        }
-      
-        
+        this.healt -= 15
     }
 }
 
@@ -31,6 +29,13 @@ class Warrior extends Character {
         if (!imgSword.classList.contains("attack")) {
             this.healt -= 20
         }
+        if(this.healt <=0){
+            
+            this.isAlive = false
+            this.die()
+            return
+        }
+       
     }
     die() {
         warriorContainer.remove()
@@ -44,9 +49,17 @@ const warrior = document.querySelector(".warriorDude")
 const warriorContainer = document.querySelector(".warrior")
 const warriroHealt = document.querySelector(".health-warrior-item")
 const GloryHealth = document.querySelector(".health_item")
-
-
-
+const invAdd = document.querySelector(".block")
+const buffEl = document.querySelector(".buff")
+let id
+function damageGlory(){
+     id = setInterval(()=>{
+        glory.damage()
+        GloryHealth.style.width = glory.healt + "%"
+    },1500)
+    return id
+}
+damageGlory()
 document.addEventListener("click", () => {
     imgSword.classList.add("attack")
 
@@ -63,8 +76,8 @@ warrior.addEventListener("click", createDamageWarrior)
 
 function createDamageWarrior() {
     warriors.damage()
-    if (warriors.healt <= 0) {
-        warriors.die()
+    if (!warriors.isAlive) {
+        clearInterval(id)
         buff.classList.add("show")
         return
     }
@@ -72,3 +85,14 @@ function createDamageWarrior() {
 
 }
 
+buffEl.addEventListener("click", () => {
+    buffEl.classList.add("toInv");
+    invAdd.append(buffEl);
+    buffEl.addEventListener("click", () => {
+        glory.healt += 30;
+        GloryHealth.style.width = glory.healt + '%'
+
+        buffEl.remove()
+    }, { once: true })
+    console.log('123')
+}, { once: true })
