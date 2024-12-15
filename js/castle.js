1a98c60c05c54de6181c5e7308a34a4335701f56
Ma1n <kskiller2020@gmail.com>
2024-12-16 01:31:18 +0200

    add spawn Buffs

--- js/castle.js
+++ js/castle.js
@@ -12,12 +12,12 @@ class Glory extends Character {
         this.inventory = []
     }
     damage() {
-        if(this.healt - 15 < 0){
+        if (this.healt - 15 < 0) {
             this.healt = 0
             this.isAlive = false
             return
         }
-        this.healt -= 15
+        this.healt -= 10
     }
 }
 
@@ -29,13 +29,13 @@ class Warrior extends Character {
         if (!imgSword.classList.contains("attack")) {
             this.healt -= 30
         }
-        if(this.healt <=0){
+        if (this.healt <= 0) {
             this.healt = 0
             this.isAlive = false
             this.die(elDelete)
             return
         }
-       
+
     }
     die(elToDel) {
         elToDel.remove()
@@ -49,29 +49,32 @@ const warrior = document.querySelector(".warriorDude")
 const warriorContainer = document.querySelector(".warrior")
 const warriroHealt = document.querySelector(".health-warrior-item")
 const GloryHealth = document.querySelector(".health_item")
-const invAdd = document.querySelector(".block")
-const buffEl = document.querySelector(".buff")
+const invAdd = document.querySelectorAll(".block")
+let score = document.querySelector(".ScoreItem") 
 const container = document.querySelector(".container")
 const loseIcon = document.querySelector(".lose")
 const spawnWarrior = document.querySelector(".spawn-warrior")
-let id
-let counterWarrior = 0
-function damageGlory(){
-     id = setInterval(()=>{
+const buffContainer = document.querySelector(".buff-container")
+let id;
+let counterWarrior = 0;
+
+
+function damageGlory() {
+    id = setInterval(() => {
         glory.damage()
         showHp(GloryHealth, glory)
-       classListAddFunc(document.body, "show-bef")
+        classListAddFunc(document.body, "show-bef")
         document.body.classList.toggle("show-after-more")
         killGlory()
 
-    },1500)
+    }, 1500)
 
     return id
 }
 damageGlory()
 container.addEventListener("click", () => {
     classListAddFunc(imgSword, "attack")
-   
+
 
 })
 setInterval(() => {
@@ -82,56 +85,59 @@ setInterval(() => {
     }
 }, 1300)
 
-warrior.addEventListener("click", ()=>{
-    createDamageWarrior(warriroHealt,warriors,warriorContainer )
+warrior.addEventListener("click", () => {
+    createDamageWarrior(warriroHealt, warriors, warriorContainer)
 })
 
+let counterInventory = 0
 
-function createDamageWarrior(warrior,character, deleteItem) {
-    
+function createDamageWarrior(warrior, character, deleteItem) {
 
-    
     character.damage(deleteItem)
     if (!character.isAlive) {
-        counterWarrior +=1
-        if(counterWarrior === 2){
+        incraseScore()
+        counterWarrior += 1
+        if (counterWarrior === 4) {
             clearInterval(id)
             classListRemoveFunc(document.body, "show-bef")
-            classListAddFunc(buff, "show")
+            classListRemoveFunc(document.body, "show-after-more")
             return
         }
         clearInterval(id)
         damageGlory()
-        classListAddFunc(buff, "show")
+
         createNewCharacter()
+
+        createBuffElement()
+        if (createBuffElement) {
+            const buffEl = buffContainer.querySelector(".buffEl")
+            counterInventory += 1
+
+            buffEl.addEventListener("click", () => {
+
+                invAdd[counterInventory - 1].append(buffEl)
+
+                buffEl.addEventListener("click", () => {
+                    glory.healt = Math.min(glory.healt + 30, 100)
+                    showHp(GloryHealth, glory)
+                    counterInventory -= 1
+                    buffEl.remove()
+                    classListAddFunc(document.body, "show-after")
+                    setTimeout(() => {
+                        classListRemoveFunc(document.body, "show-after")
+
+                    }, 1000)
+                }, { once: true })
+            }, { once: true })
+        }
 
         return
     }
     showHp(warrior, character)
 
 }
-
-buffEl.addEventListener("click", () => {
-    classListAddFunc(buffEl, "toInv")
-
-    invAdd.append(buffEl);
-    buffEl.addEventListener("click", () => {
-       glory.healt = Math.min(glory.healt + 30, 100)
-        showHp(GloryHealth, glory)
-        buffEl.remove()
-        classListAddFunc(document.body, "show-after")
-
-        setTimeout(()=>{
-            classListRemoveFunc(document.body, "show-after")
-
-        },1000)
-
-    }, { once: true })
-    console.log('123')
-}, { once: true })
-
-function killGlory(){
-    if(glory.healt <= 0){
+function killGlory() {
+    if (glory.healt <= 0) {
         classListAddFunc(loseIcon, "showLoseIcon")
 
         classListRemoveFunc(document.body, "show-bef")
@@ -142,18 +148,18 @@ function killGlory(){
     }
 }
 
-function showHp(el, charcter){
+function showHp(el, charcter) {
     el.style.width = charcter.healt + '%'
 }
 
-function classListAddFunc(el, clas){
+function classListAddFunc(el, clas) {
     el.classList.add(clas)
 }
-function classListRemoveFunc(el, clas){
+function classListRemoveFunc(el, clas) {
     el.classList.remove(clas)
 }
 
-function createNewCharacter(){
+function createNewCharacter() {
     const zalman = new Warrior("Zalman");
     spawnWarrior.insertAdjacentHTML("afterbegin", createMarkingForWarrior())
     const newWarrior = spawnWarrior.querySelector(".warrior")
@@ -161,20 +167,18 @@ function createNewCharacter(){
     const warriorDude = document.querySelector(".warriorDude")
     const NewwarriroHealt = document.querySelector(".health-warrior-item")
 
-    newWarrior.style.left = Math.floor(Math.random() * 50) + '%'
+    newWarrior.style.left = Math.floor(Math.random() * 40) + '%'
     showHp(helthWarr, zalman)
-    
-    warriorDude.addEventListener("click", ()=>{
-        createDamageWarrior(NewwarriroHealt,zalman, newWarrior)
-        
-    } )
 
+    warriorDude.addEventListener("click", () => {
+        createDamageWarrior(NewwarriroHealt, zalman, newWarrior)
 
+    })
 
 
 }
 
-function createMarkingForWarrior(){
+function createMarkingForWarrior() {
     return `
      <div class="warrior">
             <div class="health-warrior">
@@ -182,4 +186,20 @@ function createMarkingForWarrior(){
             </div>
     <img src="../img/secondWarrior-unscreen.gif" alt="" class="warriorDude">
     `
+}
+function createBuffElement() {
+    buffContainer.insertAdjacentHTML("afterbegin", createBuffMarking())
+    return true
+}
+function createBuffMarking() {
+    return `
+    <div class="buff">
+        <img src="../img/buff.gif" alt="" class="buffEl">
+    </div>
+    `
+}
+let scoreCounter = 0
+function incraseScore(){
+  scoreCounter += 40
+  score.textContent = scoreCounter
 }
