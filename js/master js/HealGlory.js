b2f2243fc4d3f2d6e4259187ec78da404bb0eecc
Ma1n <kskiller2020@gmail.com>
2024-12-24 03:11:51 +0200

    add effect heal to glory

--- js/master js/HealGlory.js
+++ js/master js/HealGlory.js
@@ -2,4 +2,8 @@ function healGlory(){
     glory.addHp(20)
     dirs.hpBar.style.width = glory.hp + "%";
     dirs.hpBarText.textContent = glory.hp;
+    classListAddFunc(document.body, "showAfter")
+    setTimeout(()=>{
+        classListRemoveFunc(document.body, "showAfter")
+    },500)
 }
