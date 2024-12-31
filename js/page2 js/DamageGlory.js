f83e8bff6c1ce73633c8114e088df8aea8c6df9b
Ma1n <kskiller2020@gmail.com>
2024-12-31 13:06:58 +0200

    add effect damage

--- js/page2 js/DamageGlory.js
+++ js/page2 js/DamageGlory.js
@@ -1,12 +1,16 @@
 import {glory} from "../FirstPageJs/DamageGlory.js"
 import { dirs } from "./dirs.js"
-import { classListAddFunc, stylehpBarWidth, styleHpBarTextContent } from "../FirstPageJs/BaseFunc.js";
+import { classListAddFunc, stylehpBarWidth, styleHpBarTextContent, classListRemoveFunc } from "../FirstPageJs/BaseFunc.js";
 import { killGlory } from "./KillGlory.js";
 function damageGlory(){
     dirs.damageInterval = setTimeout(()=>{
         glory.damage(1)
         stylehpBarWidth(dirs.healGlory,glory.hp)
         styleHpBarTextContent(dirs.textHeal, glory.hp)
+        classListAddFunc(document.body, "showBef")
+        setTimeout(()=>{
+            classListRemoveFunc(document.body, "showBef")
+        }, 1000)
     },2000)
 }
 
