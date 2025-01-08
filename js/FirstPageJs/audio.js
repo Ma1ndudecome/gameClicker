c3e593a51b006376306e8a727186e7eba62eff60
Ma1n <kskiller2020@gmail.com>
2025-01-09 01:20:29 +0200

    add new file

---
+++ js/FirstPageJs/audio.js
@@ -0,0 +1,15 @@
+export const soundHit = new Audio("../audio/hirWarriors.mp3")
+
+export const soundDringPotion = new Audio("../audio/drinkPotion.mp3")
+
+export const soundHeal = new Audio("../audio/healSound.mp3")
+
+export const soundDead = new Audio("../audio/dead.mp3")
+
+export const lose = new Audio("../audio/lose.mp3")
+
+export const pickUp = new Audio("../audio/pickUp.mp3")
+
+export const soundDragon = new Audio("../audio/dracon.mp3")
+
+export const soundDamageGragon = new Audio("../audio/dragonDamage.mp3")
